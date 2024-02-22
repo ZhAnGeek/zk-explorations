7bd7b0b4db6b78bf4e18aed662b560e6526bdfd0
ZhAnGeek <lecky.z@nodereal.io>
2024-02-22 11:43:46 +0800

    bench

--- run.sh
+++ run.sh
@@ -3,17 +3,17 @@ which $TIME
 
 cd poseidon-starky
 cargo build
-ROWS=64 RUSTFLAGS=-Ctarget-cpu=native cargo bench -- bench_poseidon_starky
-ROWS=128 RUSTFLAGS=-Ctarget-cpu=native cargo bench -- bench_poseidon_starky
-ROWS=256 RUSTFLAGS=-Ctarget-cpu=native cargo bench -- bench_poseidon_starky
-ROWS=512 RUSTFLAGS=-Ctarget-cpu=native cargo bench -- bench_poseidon_starky
-ROWS=1024 RUSTFLAGS=-Ctarget-cpu=native cargo bench -- bench_poseidon_starky
-ROWS=2048 RUSTFLAGS=-Ctarget-cpu=native cargo bench -- bench_poseidon_starky
-ROWS=4096 RUSTFLAGS=-Ctarget-cpu=native cargo bench -- bench_poseidon_starky
-ROWS=8192 RUSTFLAGS=-Ctarget-cpu=native cargo bench -- bench_poseidon_starky
-ROWS=16384 RUSTFLAGS=-Ctarget-cpu=native cargo bench -- bench_poseidon_starky
-ROWS=32768 RUSTFLAGS=-Ctarget-cpu=native cargo bench -- bench_poseidon_starky
-ROWS=65536 RUSTFLAGS=-Ctarget-cpu=native cargo bench -- bench_poseidon_starky
+ROWS=64 RUSTFLAGS=-Ctarget-cpu=native cargo bench
+ROWS=128 RUSTFLAGS=-Ctarget-cpu=native cargo bench
+ROWS=256 RUSTFLAGS=-Ctarget-cpu=native cargo bench
+ROWS=512 RUSTFLAGS=-Ctarget-cpu=native cargo bench
+ROWS=1024 RUSTFLAGS=-Ctarget-cpu=native cargo bench
+ROWS=2048 RUSTFLAGS=-Ctarget-cpu=native cargo bench
+ROWS=4096 RUSTFLAGS=-Ctarget-cpu=native cargo bench
+ROWS=8192 RUSTFLAGS=-Ctarget-cpu=native cargo bench
+ROWS=16384 RUSTFLAGS=-Ctarget-cpu=native cargo bench
+ROWS=32768 RUSTFLAGS=-Ctarget-cpu=native cargo bench
+ROWS=65536 RUSTFLAGS=-Ctarget-cpu=native cargo bench
 
 
 
